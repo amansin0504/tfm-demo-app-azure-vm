8c04f49f5b2dc6a06cc52472ff4a14cb9bd5bb10
amansin0504 <singh.amandeep@hotmail.co.in>
2023-03-08 15:36:18 -0500

    frontend vm

--- scripts/frontend.sh
+++ scripts/frontend.sh
@@ -7,7 +7,7 @@ sudo pip install Flask
 git clone https://github.com/amansin0504/tfm-demo-app-azure-vm.git
 mkdir app/
 mkdir app/templates
-cp tfm-demo-app-azure-vmsource/frontend.py app/app.py
+cp tfm-demo-app-azure-vm/source/frontend.py app/app.py
 cp tfm-demo-app-azure-vm/source/templates/index.html app/templates/
 cd app
 sudo flask run  --host=0.0.0.0 -p 8080&
